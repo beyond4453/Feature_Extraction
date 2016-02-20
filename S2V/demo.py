fea96191fa12a68035f879a8a0eb59200e6f932d
ZhipengG <beyond_Acm@163.com>
2016-02-20 14:25:02 +0800

    Add sentence To vector features

--- S2V/demo.py
+++ S2V/demo.py
@@ -16,12 +16,12 @@ from word2vec import Word2Vec, Sent2Vec, LineSentence
 logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
 logging.info("running %s" % " ".join(sys.argv))
 
-input_file = 'match_third_s2v.txt'
+input_file = 'non_match_third_s2v.txt'
 model = Word2Vec(LineSentence(input_file), size=20, window=5, sg=0, min_count=5, workers=8)
 model.save(input_file + '.model')
 model.save_word2vec_format(input_file + '.vec')
 
-sent_file = 'match_third_s2v.txt'
+sent_file = 'non_match_third_s2v.txt'
 model = Sent2Vec(LineSentence(sent_file), model_file=input_file + '.model')
 model.save_sent2vec_format(sent_file + '.vec')
 
