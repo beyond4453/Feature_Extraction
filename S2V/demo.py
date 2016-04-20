59edf61a8416e642ad4fa36f51cb44cf74474b50
ZhipengG <beyond_Acm@163.com>
2016-04-20 16:41:35 +0800

    fix an error with S2V Model

--- S2V/demo.py
+++ S2V/demo.py
@@ -16,12 +16,12 @@ from word2vec import Word2Vec, Sent2Vec, LineSentence
 logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
 logging.info("running %s" % " ".join(sys.argv))
 
-input_file = 'LDA_SOURCE.txt'
+input_file = 'TOTAL_LDA_SOURCE.txt'
 model = Word2Vec(LineSentence(input_file), size=50, window=5, sg=0, min_count=5, workers=8)
 model.save(input_file + '.model')
 model.save_word2vec_format(input_file + '.vec')
 
-sent_file = 'LDA_SOURCE.txt'
+sent_file = 'TOTAL_LDA_SOURCE.txt'
 model = Sent2Vec(LineSentence(sent_file), model_file=input_file + '.model')
 model.save_sent2vec_format(sent_file + '.vec')
 
