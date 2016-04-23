9495666964d4e77fa3d5bd8648f5eca2f481e0af
zpGao <beyond_acm@163.com>
2016-04-24 01:08:31 +0800

    use topic model & s2v model to generate features

--- S2V/demo.py
+++ S2V/demo.py
@@ -16,13 +16,13 @@ from word2vec import Word2Vec, Sent2Vec, LineSentence
 logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
 logging.info("running %s" % " ".join(sys.argv))
 
-input_file = 'TOTAL_LDA_SOURCE.txt'
-model = Word2Vec(LineSentence(input_file), size=50, window=5, sg=0, min_count=5, workers=8)
-model.save(input_file + '.model')
-model.save_word2vec_format(input_file + '.vec')
+# input_file = 'TOTAL_LDA_SOURCE.txt'
+# model = Word2Vec(LineSentence(input_file), size=50, window=5, sg=0, min_count=5, workers=8)
+# model.save(input_file + '.model')
+# model.save_word2vec_format(input_file + '.vec')
 
 sent_file = 'TOTAL_LDA_SOURCE.txt'
-model = Sent2Vec(LineSentence(sent_file), model_file=input_file + '.model')
+model = Sent2Vec(LineSentence(sent_file), model_file='S2V.model')
 model.save_sent2vec_format(sent_file + '.vec')
 
 program = os.path.basename(sys.argv[0])
